f59417e1bb97fd04ba10da943f531e8d80cb37ca
AM021311 <s92lily@gmail.com>
2015-04-29 16:24:03 +0800

    A AM021311 王馨儀提交 ThreadA

--- Question3.java
+++ Question3.java
@@ -1,5 +1,20 @@
-﻿public class Question3{
-	public static void main(String[] args){
-	
-	}
+﻿public class Question
+  public static void main(String[] args){
+    HellowThread t1 = new HelloThread();
+    HellowThread t2 = new HelloThread();
+    ThreadA t1 = new ThreadA(t1, "T1");
+    ThreadA t2 = new ThreadA(t2, "T2");
+    t1.start();
+    t2.start();
+    System.out.println(Thread.activeCount()); 
+  }
+}
+class HelloThread implements ThreadA{
+  public void run(){
+    for(int i=1; i<1000; i++){ 
+      String tName = ThreadA.currentThread().getName();       
+	System.out.println("ThreadA"); 
+	ThreadA.sleep(1000);
+    } 
+  } 
 }
